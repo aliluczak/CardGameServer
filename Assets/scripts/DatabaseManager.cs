89ba64bf9c8aa88acee700e05195cc1931929484
362669 <kierownik_imprezy@onet.eu>
2015-05-27 12:18:55 +0200

    próby z bazą

--- Assets/scripts/DatabaseManager.cs
+++ Assets/scripts/DatabaseManager.cs
@@ -16,9 +16,42 @@ public class DatabaseManager : MonoBehaviour {
     internal IDataReader _dbr;
 
 	// Use this for initialization, initializes all needed gameobjects and components
-	void Start () {
-	
-	}
+    void Start()
+    {
+
+        string sql;
+        string _constr = "URI=file:" + Application.dataPath + "/DataBase.db"; //Path to database.
+        IDbConnection _dbc;
+        IDbCommand _dbcm;
+        IDataReader _dbr;
+
+        _dbc = new SqliteConnection(_constr);
+        _dbc.Open(); //Open connection to the database.
+        _dbcm = _dbc.CreateCommand();
+        sql = "SELECT Nick, Wygrane, Remisy, Przegrane FROM Gracze order by Nick desc";
+        _dbcm.CommandText = sql;
+        _dbr = _dbcm.ExecuteReader();
+
+        // lista (Ranking) graczy
+        while (_dbr.Read())
+        {
+            string Nick = _dbr.GetString(1);
+            string Wygrane = _dbr.GetString(4);
+            string Remisy = _dbr.GetString(5);
+            string Przegrane = _dbr.GetString(6);
+            //Console.WriteLine(&amp;amp;quot;Name: &amp;amp;quot; + Nick + &amp;amp;quot; &amp;amp;quot; + Wygrane);
+            Debug.Log(Nick + Wygrane + Remisy + Przegrane);
+        }
+
+        // clean up
+        _dbr.Close();
+        _dbr = null;
+        _dbcm.Dispose();
+        _dbcm = null;
+        _dbc.Close();
+        _dbc = null;
+
+    }
 
     //TODO function connecting to database, probably use parameter
     internal void connectToDatabase()
